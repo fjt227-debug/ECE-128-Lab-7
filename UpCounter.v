6e76c8e437914c8b656383a93735606a05ef9af8
alh522 <alh522@lehigh.edu>
2025-11-13 16:06:11 -0500

    UpCounter.v

--- UpCounter.v
+++ UpCounter.v
@@ -1,41 +1,24 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/29/2025 03:13:47 PM
-// Design Name: 
-// Module Name: UpCounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module UpCounter(
-input wire clk,
-input wire rst,
-input wire en,
-output reg [11:0] q);
+module UpCounter(clk,reset,count);
+input clk, reset;
+output reg [11:0] count;
 
-always @(posedge clk) begin
-    if (rst) begin
-        q <= 12'd0;
+reg [11:0] next_count = 12'd1;
+always@(posedge clk)begin
+    if(reset)begin
+        count<=12'd0;
     end
-    else if (en) 
-    begin
-            if (q == 12'd4095)
-                q <= 12'd0;
-            else
-                q <= q + 12'd1;
-        end 
+    else begin
+      count<=next_count;  
     end
+end
+
+always@(*)begin
+    if(count == 12'd4095)
+        next_count=12'd0;
+    else
+        next_count = count + 1;
+end
 endmodule
+
