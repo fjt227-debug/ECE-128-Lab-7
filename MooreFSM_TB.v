1a67772608bce4bd56af17cdfc1e0f9cafd17de8
alh522 <alh522@lehigh.edu>
2025-11-13 15:58:42 -0500

    MooreFSM_TB.v

--- MooreFSM_TB.v
+++ MooreFSM_TB.v
@@ -1,4 +1,3 @@
-
 module MooreFSM_TB();
 reg p1,clk, reset;
 wire z;
@@ -6,24 +5,30 @@ MooreFSM uut (.p1(p1),.clk(clk), .reset(reset), .z(z));
 
 initial begin
 clk = 0;
-forever #10 clk = ~clk;
+forever #5 clk = ~clk;
 end
 
 initial begin
     reset = 1; 
     p1 = 0;
-    #25;
-     reset = 0;
-    #20 p1 = 1;   
-    #20 p1 = 1;   
-    #20 p1 = 0;   
-    #20 p1 = 1;   
-    #20 p1 = 0;   
-    #20 p1 = 1;   
-    #20 p1 = 0;   
+    #10;
+    reset = 0;
+    #10 p1 = 1;   
+    #10 p1 = 1;   
+    #10 p1 = 0;   
+    #10 p1 = 0;
+    #10 reset = 1; 
+    #10 reset = 0;  
+    #10 p1 = 1;
+    #10 p1 = 1;   
+    #10 p1 = 0;   
+    #10 p1 = 0; 
+    
+      
 
-     
+    #10;
     $finish;
     
 end
 endmodule
+
