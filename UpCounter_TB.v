297dea6fb05e17e57dcae81da2cc4381e3a226bd
alh522 <alh522@lehigh.edu>
2025-11-13 16:07:03 -0500

    UpCounter_TB.v

--- UpCounter_TB.v
+++ UpCounter_TB.v
@@ -1,67 +1,29 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/29/2025 05:06:45 PM
-// Design Name: 
-// Module Name: UpCounter_TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module UpCounter_TB();
-    reg clk;
-    reg rst;
-    reg en;
-    wire [11:0] q;
-    
-    UpCounter DUT(.clk(clk),.rst(rst),.en(en),.q(q));
-    initial begin
-        clk = 0;
-        forever #10 clk = ~clk;
-    end
-    
-    initial begin
-        rst = 1;
-        en =0;
+reg clk, reset;
+wire [11:0]count;
+
+UpCounter uut(.clk(clk), .reset(reset), .count(count));
+initial begin
+clk = 0;
+forever #10 clk = ~clk;
+end
+
+initial begin
+        reset = 1;
         
         @(posedge clk);
-        rst = 0;
-        
-        @(posedge clk); en = 1;
-        @(posedge clk); en = 0;
-        
-        @(posedge clk); en = 1;
-        @(posedge clk); en = 0;
-        
-        @(posedge clk); en = 1;
-        @(posedge clk); en = 0;
-        
+        reset = 0;
+        @(posedge clk); reset=1;
         @(posedge clk);
-        @(posedge clk);
+        reset = 0;
+
         
-        @(negedge clk); rst=1;
-        @(posedge clk);
-        @(negedge clk); rst=0;
-        
-        @(posedge clk); en = 1;
-        @(posedge clk); en = 0;
-        
-        @(posedge clk); en = 1;
-        @(posedge clk); en = 0;
+  
         
         #50;
         $finish;
      end    
+
 endmodule
+  
